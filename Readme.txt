a6dcfb9eb615d07af311701a2cbaa0bfbc6f3c8a
jsvan <bitsinthesky@gmail.com>
2020-11-07 12:45:27 -0500

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,107 @@
+# CommonSents
+
+*( This is the latest version of Common Sents if you want the best black market app available. )*
+
+
+
+# **Welcome to Julian's Common Sents, the media sentiment collector!**
+
+
+## Please take a moment to acquaint yourself with the idea behind my project and the How-Tos by reading through the following short presentations.
+
+<div class="fdiv">
+
+<div class="indiv">
+
+**Contents:**
+
+1.  Introduction
+2.  Slideshow: A Philosophy Towards Common Sents. An introduction to the project, and guide to the decision making necessary for annotation.
+3.  Slideshow: How to Use Common Sense. A guide on the tool mechanics and design, for your use as you read articles online."
+4.  Testimonials
+
+</div>
+
+</div>
+
+<div class="fdiv">
+
+<div class="indiv">
+
+**Introduction:**
+
+<div style="padding: 20px;">
+
+Welcome! This is a tool primarily to help Julian collect a dataset. The first slides introduce what he's doing and why this is a great project.
+
+It should also be helpful to you as an educational tool. At your discretion, you will begin to slow down and process what you are reading in a new way. You will analyze the sentences and look for sentiment queues which have been inserted to mold your opinion on the given topic at a deeper level than you may otherwise consciously notice.
+
+The second slide show will introduce you to how to use this extension. In short, it requires you to highlight and then hit a hotkey to triage and store interesting sentences that you come across.
+
+This app is not working behind the scenes to process anything that you're seeing... yet. Before I can get that kind of cool functionality I need some humans to do some of the heavy lifting for me.
+
+There are some extremely cool ideas I have to inject some broad analysis into your news pages, but first gotta start with the low tech stuff <3
+
+**Note on foreign languages:** It is all right to annotate in multiple languages. Each sample we're sent is individually automatically translated into English, so it's all good.
+
+**Excitement for _next week's_ update:** <mark style="background-color:yellow">We're getting <mark style="background-color:lightgreen">text-highlighting capabilities</mark> so you'll immediately know what you've notated! It's not perfect, but ill tell you more when Chrome finally accepts the update next week...</mark>
+
+</div>
+
+</div>
+
+</div>
+
+<div class="fdiv">
+
+<div class="indiv">
+
+**Slideshow 1/2:**
+
+*   The idea behind the project, inspiration and importance.
+
+<div style="padding:16px">https://docs.google.com/presentation/d/e/2PACX-1vSbqv3MBVHStBgwmQee-cBwe5ZPOtJQtOGo9H5TU0Pt29WQLzOApj9pBumajBjspe7nGIOGKbMWaCuu/embed?start=false&amp;loop=false&amp;delayms=5000</div>
+
+</div>
+
+</div>
+
+<div class="fdiv">
+
+<div class="indiv">
+
+**Slideshow 2/2:**
+
+*   Instructions on exactly how to use the extension.
+
+<div style="padding:16px">https://docs.google.com/presentation/d/e/2PACX-1vS_cezln_4AmUcGwFQhOmZ-UZ9EdIVoQeOsK4-siUZ0LVDgn50n6dhZZy23GkMy_xVpmCR8TrRfAHZJ/embed?start=false&amp;loop=false&amp;delayms=5000</div>
+
+</div>
+
+</div>
+
+<div class="fdiv">
+
+<div class="indiv">
+
+**Testimonials:**
+
+*   _"This is actually super fun to do with legal articles because these are supposed to be neutral and so they mostly are, but when you read them piece by piece like you have to do in order to code them, it really highlights how legal writing can very powerfully shape your understanding of an issue through subtle sentiment markers."_
+
+    <pre>        - Anya Vanecek </pre>
+
+</div>
+
+</div>
+
+## I appreciate all your help. And now you may read the news for science.
+
+It's still a work in progress so hit me up with suggestions and questions.
+
+[Please share this with your smart friends! The more people sending me odd samples the better :)](https://chrome.google.com/webstore/detail/common-sents/bmhhlclpcciacjfgigihpkaeggmgkgih)
+
+Julian Vanecek
+University of Maryland College Park
+jsvsentilabel gmail com
+
+* * *
---
+++ highlightpage.js
@@ -0,0 +1,138 @@
+var CONTEXTCOLOR = "yellow"
+var NEGCOLOR = "red"
+var POSCOLOR = "lightgreen"
+var NEUCOLOR = "lightgray"
+var prevContextStart = 0;
+var prevContextEnd = 0;
+var FLASHDURATION = 50;
+var ALERTCOLOR = "red";
+alltaglocs = []
+
+function setprevContextStart(num){
+    prevContextStart = num;
+    console.log("prevCon set to ", prevContextStart);
+}
+
+function setprevContextEnd(num){
+    prevContextEnd = num;
+    console.log("prevCon set to ", prevContextEnd);
+    console.log("Context now [" + document.body.innerHTML.substring(prevContextStart, prevContextEnd) + "]");
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    console.log(request.action + " " + request.highlighted);
+    var HL = request.highlighted.trim();
+
+    if (request.action == "con") {
+      addContext(HL);
+    } else if (request.action == "pos"){
+        addPos(HL);
+    } else if (request.action == "neu"){
+        addNeu(HL);
+    } else if (request.action == "neg"){
+        addNeg(HL);
+    } else if (request.action == "und"){
+        undo();
+    } else if (request.action == "not"){
+        notify(HL);
+    } else {
+        console.log(request.action + " not found.");
+    }
+  });
+
+
+function notify() {
+    var oldcolor = document.body.style.backgroundColor;
+    innernotify(oldcolor);
+    setTimeout(function() {innernotify(oldcolor)}, FLASHDURATION+60);
+    setTimeout(function() {innernotify(oldcolor)}, 2*FLASHDURATION+70);
+    setTimeout(function() {revertscreen(oldcolor)}, 3*FLASHDURATION+80);
+};
+
+function innernotify(oldcolor){
+    flashscreen();
+    console.log("flashed");
+    setTimeout(function() {revertscreen(oldcolor)}, FLASHDURATION);
+    console.log('done');
+};
+
+function flashscreen(){
+    document.body.style.backgroundColor = ALERTCOLOR;
+};
+
+function revertscreen(color){
+    document.body.style.backgroundColor = color;
+};
+
+function markS(color){
+    var builtstring = " <mark id=\"JSVCS\" style=\"background-color:" + color + "\"> ";
+    setprevContextEnd(prevContextEnd + builtstring.length)
+    return [builtstring, builtstring.length];
+};
+
+function markE(){
+    var builtstring = " </mark> ";
+    setprevContextEnd(prevContextEnd + builtstring.length)
+    return [builtstring, builtstring.length];
+};
+
+function addContext(highlighted){
+    var body = document.body.innerHTML;
+    console.log("CONTEXT finding [" + highlighted + "]");
+    try{
+        var start = body.match(highlighted).index;
+        var end = start + highlighted.length;
+    } catch (e){
+        alltaglocs.push([[0,0], [0,0]]);
+        return;
+    }
+
+    setprevContextStart(start);
+    setprevContextEnd(end);
+    var smark = markS(CONTEXTCOLOR);
+    var emark = markE();
+    var newbody = body.substring(0, start) + smark[0] + body.substring(start, end) + emark[0] + body.substring(end);
+
+    document.body.innerHTML = newbody;
+    alltaglocs.push([ [start, start+smark[1] ] ,[ end+smark[1], end+smark[1]+emark[1]] ])
+};
+
+function addCol(highlighted, color){
+    var body = document.body.innerHTML;
+    console.log("finding " + highlighted + " on indexes " + prevContextStart.toString() + " : " + prevContextEnd.toString() + " in ["+
+                body.substring(prevContextStart, prevContextEnd) + "] .")
+    var start = prevContextStart + body.substring(prevContextStart, prevContextEnd).match(highlighted).index;
+    var end = start + highlighted.length;
+    var smark = markS(color);
+    var emark = markE();
+    var newbody = body.substring(0, start) + smark[0] + body.substring(start, end) + emark[0] + body.substring(end);
+
+    alltaglocs.push([ [start, start+smark[1] ] , [ end+smark[1], end+smark[1]+emark[1] ] ])
+    document.body.innerHTML = newbody;
+};
+
+function addPos(highlighted) {
+    addCol(highlighted, POSCOLOR);
+};
+
+function addNeg(highlighted) {
+    addCol(highlighted, NEGCOLOR);
+};
+
+function addNeu(highlighted) {
+    addCol(highlighted, NEUCOLOR);
+};
+
+function undo() {
+    var body = document.body.innerHTML;
+    var toremove = alltaglocs.pop();
+    var first = toremove[0];
+    var second = toremove[1];
+    var newbody = body.substring(0, first[0]) + body.substring(first[1], second[0]) + body.substring(second[1]);
+    document.body.innerHTML = newbody;
+    setprevContextEnd(prevContextEnd - (first[1]-first[0]) - (second[1] - second[0]));
+};
